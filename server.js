d535e68edc9efeadd486cdfd7aa5e31395083ef8
cjswayne <134882908+cjswayne@users.noreply.github.com>
2024-01-28 12:14:03 -0800

    added p to auto open

--- server.js
+++ server.js
@@ -8,6 +8,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
     console.log('Server started on port', PORT);
+    // Use dynamic import for the 'open' package
+    import("open")
+        .then((open) => {
+            const readline = require("readline");
+
+            // URL you want to open
+            const url = `http://localhost:${PORT}/`;
+
+            console.log("Press 'p' to open in browser");
+
+            // Listen for keyboard input
+            readline.emitKeypressEvents(process.stdin);
+            process.stdin.setRawMode(true);
+            process.stdin.on("keypress", (str, key) => {
+                if (key.name === "p") {
+                    // Open the URL in the default browser
+                    open.default(url); // Note the use of 'open.default' due to the way ES modules are imported
+                } else if (key.sequence === "\u0003") {
+                    // This is CTRL+C
+                    // Exit the program
+                    process.exit();
+                }
+            });
+        })
+        .catch((err) => console.error("Failed to load the open module", err));
 })
 
-// work on getting all the question prompts done, then add in submit button and hook everything up to the backend
